3cc18ecff127b9cac14e46f154f4332ae21eccd3
brchen777 <eric790620@gmail.com>
2017-05-28 17:03:55 +0800

    檔案調整

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>結帳</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
 </head>
 <body>
 <div class="title">DVD 結帳系統</div>
 <form action="main.php" method="post">
-    <div class="movie_num">紅標 <input type="text" name="red"> 片</div>
-    <div class="movie_num">綠標 <input type="text" name="green"> 片</div>
-    <div class="movie_num">藍標 <input type="text" name="blue"> 片</div>
+    <div class="dvd_count_num">紅標 <input type="text" name="red"> 片</div>
+    <div class="dvd_count_num">綠標 <input type="text" name="green"> 片</div>
+    <div class="dvd_count_num">藍標 <input type="text" name="blue"> 片</div>
     <div><button type="submit">計算</button></div>
 </form>
 </body>
